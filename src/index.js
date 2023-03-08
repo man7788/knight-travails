1c6ec142ff4d0bd9da1d3116b9925ec4cc4972a2
man7788 <jerman1841@gmail.com>
2023-03-08 10:02:18 +0800

    Upadate knightMoves

--- src/index.js
+++ src/index.js
@@ -2,21 +2,121 @@ import './style.css';
 import { Knight } from './knight';
 import { makeTree } from './move-tree';
 
-const knightMoves = (start, end) => {
-  // Make two tree
-  const startTree = makeTree(start);
-  const endTree = makeTree(end);
-  // Find node closeset to end node (difference in coord value)
-  // Find way to end node moves as target
-  console.log(startTree, endTree);
+const preorder = (node, end, child = [], match = []) => {
+  if (node === null) {
+    return;
+  }
+
+  if (node.data[0] !== end[0] || node.data[1] !== end[1]) {
+    child.push(node.data);
+  }
+
+  if (node.data[0] === end[0] && node.data[1] === end[1]) {
+    match.push(node.data);
+  }
+
+  if (match.length > 0) {
+    return match;
+  }
+
+  preorder(node.left, end, child, match);
+  preorder(node.right, end, child, match);
+
+  return child;
 };
 
-// Outer cross = 2 steps: 1 knight, 1 end
-// Adjacent = 3 steps: 1 normal, 1 knight, 1 end
-// Diagonal = 2 steps: 1 knight, 1 end
+// When match found, recusively use knightMovs to find all steps
+const knightMoves = (
+  start,
+  end,
+  genesis = [],
+  memory = {},
+  temp = [],
+  currentLvl = 0,
+  currentData = 0
+) => {
+  console.log(start);
+  // New level of steps finish searching, no matching found
+  // Continue to next level
+  if (start === undefined) {
+    console.log('end');
+    currentLvl += 1;
+    currentData = 0;
+    const searchLvl = `level${currentLvl}`;
+    memory[searchLvl] = temp;
+    temp = [];
+    return knightMoves(
+      memory[searchLvl][currentData],
+      end,
+      genesis,
+      memory,
+      temp,
+      currentLvl,
+      currentData
+    );
+  }
 
-knightMoves([2, 4], [6, 7]);
-// console.log(Knight([6, 7]));
+  const startTree = makeTree(start);
+  const startMoves = preorder(startTree, end);
 
-// const on9 = makeTree([0, 0]);
-// console.log(on9);
+  // Found match, continue to search for match's parent step
+  if (startMoves[0][0] === end[0] && startMoves[0][1] === end[1]) {
+    const firstHalf = [genesis[0]];
+    firstHalf.push(start);
+    const secondHalf = genesis.slice(1, genesis.length);
+    genesis = firstHalf.concat(secondHalf);
+    memory = {};
+    temp = [];
+    currentLvl = 0;
+    currentData = 0;
+    return genesis;
+    // return console.log(genesis[0], genesis[1]);
+    // if (genesis[1][0] === end[0] && genesis[1][1] === end[1]) {
+    //   return genesis;
+    // }
+    // return knightMoves(
+    //   genesis[0],
+    //   genesis[1],
+    //   genesis,
+    //   memory,
+    //   temp,
+    //   currentLvl,
+    //   currentData
+    // );
+  }
+
+  // Record next level of temporary search memory
+  if (Object.keys(memory).length > 0) {
+    console.log('temp');
+    startMoves.forEach((coord) => {
+      temp.push(coord);
+    });
+  }
+
+  // Record first level of search memory
+  if (Object.keys(memory).length === 0) {
+    console.log('length0');
+    genesis.push(start, end);
+    memory.level0 = startMoves;
+  }
+
+  // Go to next step in memory
+  const searchLvl = `level${currentLvl}`;
+
+  return knightMoves(
+    memory[searchLvl][currentData],
+    end,
+    genesis,
+    memory,
+    temp,
+    currentLvl,
+    currentData + 1
+  );
+  // return memory[seachLvl][currentData];
+  // return temp;
+  // return startMoves;
+};
+
+const diu = knightMoves([2, 2], [4, 4]);
+console.log(diu);
+console.log(Knight([5, 2]));
